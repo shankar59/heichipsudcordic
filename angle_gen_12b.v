df27f4e12193a5221575cedc1fd31c0b4a6569e3
Shankaranarayanan H <58130404+shankar59@users.noreply.github.com>
2025-08-07 23:45:22 +0530

    Update angle_gen_12b.v

--- angle_gen_12b.v
+++ angle_gen_12b.v
@@ -2,45 +2,47 @@
 
 
 
-module angle_cordic_12b#(parameter width = 12, CNT = 65536, freq_width = 16) (clock, resetn, freq, SINout, COSout,angleout);
+module angle_gen_12b#(parameter width = 12, CNT = 524288, freq_width = 12) (clock, resetn, freq, angle, x_start, y_start);
 
   
+
 // Inputs
   input clock;
   input resetn;
   input [freq_width-1:0] freq;
-  
-  output reg signed [width-1:0] SINout;
-  output reg signed [width-1:0] COSout;
-  output reg signed [width-1:0] angleout;
-  wire [width-1:0] x_start,y_start;
-  
-  wire [width-1:0] angle;
-  
-  wire [width-1:0] SINout_wire;
-  
-  wire [width-1:0] COSout_wire;
-  
-  angle_gen_12b#(.width(width), .CNT(CNT), .freq_width(freq_width)) angle_gen(.clock(clock), .resetn(resetn), .freq(freq), .angle(angle), .x_start(x_start), .y_start(y_start));
-  
- cordic_12b#(.width(width)) cordic(.clk(clock), .resetn(resetn), .SINout(SINout_wire), .COSout(COSout_wire), .x_start(x_start), .y_start(y_start), .angle(angle));
  
-  always @ (posedge clock)
+//Outputs
+	output reg  [width-1:0] x_start,y_start; 
+	output reg  [width-1:0] angle;
+    
+    wire [width-1:0] An = 1215; //2000*0.6073
+	
+	reg [freq_width-1:0] freq_reg;
+	
+	reg [freq_width:0] cnt;
+	
+	wire [freq_width+7:0] cnt_sum = CNT-(freq_reg<<7);
+	
+	always @ (posedge clock)
 		begin
-			SINout <= (!resetn) ? 0 : SINout_wire;
-			COSout <= (!resetn) ? 0 : COSout_wire;
-			angleout <= (!resetn) ? 0 : angle;
-		end
-		
-//ila_0 ila (
-//	.clk(clock), // input wire clk
+			freq_reg <= (!resetn) ? 0 : freq;
 
-
-//	.probe0(resetn), // input wire [0:0]  probe0  
-//	.probe1(start), // input wire [0:0]  probe1 
-//	.probe2(freq), // input wire [15:0]  probe2 
-//	.probe3(angle), // input wire [11:0]  probe3 
-//	.probe4(SINout) // input wire [11:0]  probe4
-//);		
-		
+		end	
+	
+	
+	always @ (posedge clock)
+	begin
+		cnt <= (!resetn) ? 0: (cnt == cnt_sum) ? 0 : cnt+1'b1;
+	end
+	
+	always @ (posedge clock)
+	begin
+		angle <= (!resetn) ? 0 : (cnt == cnt_sum) ? angle +12'h07F : angle;
+	end
+	
+	always @ (posedge clock)
+	begin
+		x_start <= (!resetn) ? 0 : An;
+		y_start <= (!resetn) ? 0 : 0;
+	end
 endmodule
